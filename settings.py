0bdd13b8c25146b147e4e2e73d857685e4a7aa4f
Roderick Randolph <me@roderickrandolph.com>
2014-06-17 23:08:28 -0400

    convert to eve framework

---
+++ settings.py
@@ -0,0 +1,50 @@
+people = {
+    # 'title' tag used in item links.
+    'item_title': 'person',
+
+    # by default the standard item entry point is defined as
+    # '/people/<ObjectId>/'. We leave it untouched, and we also enable an
+    # additional read-only entry point. This way consumers can also perform GET
+    # requests at '/people/<lastname>/'.
+    'additional_lookup': {
+        'url': 'regex("[\w]+")',
+        'field': 'lastname'
+    },
+
+    'schema': {
+        'firstname': {
+            'type': 'string',
+            'minlength': 1,
+            'maxlength': 10,
+        },
+        'lastname': {
+            'type': 'string',
+            'minlength': 1,
+            'maxlength': 15,
+            'required': True,
+            # talk about hard constraints! For the purpose of the demo
+            # 'lastname' is an API entry-point, so we need it to be unique.
+            'unique': True,
+        },
+        # 'role' is a list, and can only contain values from 'allowed'.
+        'role': {
+            'type': 'list',
+            'allowed': ["author", "contributor", "copy"],
+        },
+        # An embedded 'strongly-typed' dictionary.
+        'location': {
+            'type': 'dict',
+            'schema': {
+                'address': {'type': 'string'},
+                'city': {'type': 'string'}
+            },
+        },
+        'born': {
+            'type': 'datetime',
+        },
+    }
+}
+
+DOMAIN = {
+    'people': people,
+}
